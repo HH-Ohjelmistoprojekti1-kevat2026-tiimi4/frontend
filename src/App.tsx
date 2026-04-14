8a1f203c0ac89d6cadb3a0e6ee73f3260f47a33e
Wuggynaut <arimatti.toivonen@gmail.com>
2026-04-14 15:19:15 +0300

    Navigaatio pohjustettu

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Navbar from "./components/Navbar.tsx";
+import Homepage from "./components/Homepage.tsx";
 
 function App() {
 
   return (
     <>
-
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+      </Routes>
     </>
   )
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,8 @@
+
+export default function Navbar() {
+    return (
+        <>
+            {/-- Tänne tulee navigaatio --/}
+        </>
+    )
+}
