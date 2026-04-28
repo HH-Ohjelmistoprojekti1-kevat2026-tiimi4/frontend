4e1dca5f12b563347e770f37ea562beac6ad6d17
Wuggynaut <arimatti.toivonen@gmail.com>
2026-04-28 14:29:36 +0300

    Layout ja navbar implementaatio

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import Homepage from "./components/Homepage.tsx";
+import Layout from "./components/Layout.tsx";
 
 function App() {
 
   return (
     <>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+          <Route element={<Layout/>}>
+              <Route path="/" element={<Homepage />} />
+          </Route>
       </Routes>
     </>
   )
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,16 @@
+import style from './Homepage.module.css'
+import {Link} from "react-router-dom";
 
 export default function Navbar() {
     return (
         <>
-            {/* Tänne tulee navigaatio */}
+            <header className={style.topbar}>
+                <div className={style.brand}>LEMMIKKIPUOTI</div>
+                <nav className={style.nav} aria-label="Päävalikko">
+                    <Link className={style.navLink} to="/meista">Meistä</Link>
+                    <Link className={style.navLink} to="/tuotteet">Tuotteet</Link>
+                </nav>
+            </header>
         </>
     )
 }
