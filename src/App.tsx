da8be715638b8e546d06d69a1f15c8c32f727f1b
Wuggynaut <arimatti.toivonen@gmail.com>
2026-04-28 18:03:27 +0300

    product page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import Homepage from "./components/Homepage.tsx";
 import Layout from "./components/Layout.tsx";
 import Manufacturer from './components/Manufacturer.tsx';
+import ProductsPage from "./components/ProductsPage.tsx";
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
           <Route element={<Layout/>}>
               <Route path="/" element={<Homepage />} />
               <Route path="/manufacturer" element={<Manufacturer />} />
+              <Route path="/tuotteet" element={<ProductsPage/>} />
           </Route>
       </Routes>
     </>
