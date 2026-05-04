feae29b89f7b30c4a7afd34fc057501b59c899d7
sawasda <sara.junnila@haaga-helia.fi>
2026-05-04 11:49:35 +0300

    Router-korjaus

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,18 @@ import { Routes, Route } from 'react-router-dom'
 import Homepage from "./components/Homepage.tsx";
 import Layout from "./components/Layout.tsx";
 import ProductsPage from "./components/ProductsPage.tsx";
+import About from './components/About.tsx';
 
 function App() {
 
   return (
     <>
       <Routes>
-          <Route element={<Layout/>}>
-              <Route path="/" element={<Homepage />} />
-              <Route path="/tuotteet" element={<ProductsPage/>} />
-          </Route>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/tuotteet" element={<ProductsPage />} />
+          <Route path="/meista" element={<About />} />
+        </Route>
       </Routes>
     </>
   )
