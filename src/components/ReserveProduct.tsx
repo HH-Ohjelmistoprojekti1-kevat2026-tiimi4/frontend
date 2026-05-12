8ef725676cbcc93bd4fcca72e38763e15bbd7c50
sawasda <sara.junnila@haaga-helia.fi>
2026-05-12 11:47:44 +0300

    Varaussivun CSS

--- src/components/ReserveProduct.tsx
+++ src/components/ReserveProduct.tsx
@@ -5,24 +5,32 @@ export default function ReserveProduct() {
 
     const location = useLocation();
     const product = location.state?.product;
-    
+
     if (!product) {
         return <p>Tuotetta ei löytynyt.</p>;
     }
-    
+
     const user = localStorage.getItem("user");
 
     return (
         <main className={style.page}>
             <h1>Varaa tuote</h1>
-            <h2>Tuotteen tiedot</h2>
-            <p>Nimi: {product.name}</p>
-            <p>Väri: {product.color}</p>
-            <p>Koko: {product.size}</p>
-            <p>Hinta: {product.price}</p>
-            <p>Valmistaja: {product.manufacturer.name}</p>
-            <h2>Varaajan tiedot</h2>
-            <p>Sähköposti: {user}</p>
+            <div className={style.infoSection}>
+                <h3>Tuotteen tiedot</h3>
+                <div className={style.infoBox}>
+                    <p>Nimi: {product.name}</p>
+                    <p>Väri: {product.color}</p>
+                    <p>Koko: {product.size}</p>
+                    <p>Hinta: {product.price}</p>
+                    <p>Valmistaja: {product.manufacturer.name}</p>
+                </div>
+            </div>
+            <div className={style.infoSection}>
+                <h3>Varaajan tiedot</h3>
+                <div className={style.infoBox}>
+                    <p>Sähköposti: {user}</p>
+                </div>
+            </div>
             <button className={style.cta}>Vahvista varaus</button>
         </main>
     )
